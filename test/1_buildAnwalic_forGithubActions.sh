e14d950b3f4d3b7ce7f30b3f635feb289e7e5233
gre90r <gregor_hoben@live.de>
2020-09-11 16:57:04 +0200

    fix CI

--- test/1_buildAnwalic_forGithubActions.sh
+++ test/1_buildAnwalic_forGithubActions.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+# Qt5_DIR: /home/runner/work/Anwalic/Anwalic/Qt/5.14.2/gcc_64
 
 # Qt bin directory is added to Path by github workflow install-qt-action
 QMAKE=qmake
 
-cd ..
-$QMAKE anwalic.pro -spec linux-g++
+$QMAKE ../anwalic.pro -spec linux-g++
 make
